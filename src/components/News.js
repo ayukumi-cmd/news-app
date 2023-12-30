8514f6de5b85a06f16e99eb3a19fc8baa698ac09
ayush <ayushmi274@gmail.com>
2023-12-30 16:37:48 +0530

    latest commit

--- src/components/News.js
+++ src/components/News.js
@@ -1,18 +1,55 @@
-import Items from "../components/Items";
-import '../assets/news.css';
-import useFetch from "../hooks/useFetch";
-const apiKey = process.env.REACT_APP_NEWS_API;
+// import React from 'react'
+// import Items from "../components/Items";
+// import '../assets/news.css';
+// import useFetch from "../hooks/useFetch";
+// const apiKeys=process.env.REACT_APP_NEWS_API;
+// const News = () => {
+//   // Used custom hook useFetch to fetch the data
+//   const {articles} = useFetch(`/top-headlines?country=in&apiKey=${apiKeys}`);
+//   return (
+//     <div className="container-fluid">
+//       <h1 className="headlines">Top Headlines</h1>
+//       <div className="row row-cols-1 row-cols-md-3 row-eq-height" >
+//         {articles.map((article) => {
+//           return (
+//             <div className="col-sm-4  d-flex align-items-stretch" key={article.id}>
+//             <Items
+//               title={article.title}
+//               description={article.description}
+//               url={article.url}
+//               urlToImage={article.urlToImage}
+//               publishedAt={article.publishedAt}
+//               author={article.author}
+//               source={article.source.name}z
+//             />
+//             </div>
+//           );
+//         })}
+//       </div>
+//     </div>
+//   );
+//   //}
+// };
+// export default News;
+import React from 'react'
+import Items from './Items';
+import '../assets/news.css'
+import useFetch from '../hooks/useFetch';
+
+
 const News = () => {
   // Used custom hook useFetch to fetch the data
-  const {articles} = useFetch(`/top-headlines?country=in&apiKey=${apiKey}`);
+  const {articles} = useFetch(`/top-headlines?country=in&apiKey=70123fc1a2d542c8911f05f40a131526`);
   
+
   return (
+      
     <div className="container-fluid">
-      <h1 className="headlines">Top Headlines</h1>
-      <div className="row row-cols-1 row-cols-md-3 row-eq-height" >
+        <h1 className="headlines">Top News Headlines</h1>
+    <div className="row row-cols-1 row-cols-md-3 row-eq-height" >
         {articles.map((article) => {
           return (
-            <div className="col-sm-4  d-flex align-items-stretch" key={article.id}>
+            <div className="col sm-4  d-flex align-items-stretch">
             <Items
               title={article.title}
               description={article.description}
@@ -20,14 +57,16 @@ const News = () => {
               urlToImage={article.urlToImage}
               publishedAt={article.publishedAt}
               author={article.author}
-              source={article.source.name}z
+              source={article.source.name}
             />
             </div>
           );
+          
         })}
       </div>
-    </div>
-  );
-  //}
-};
+      </div>
+
+  )
+}
+
 export default News;
